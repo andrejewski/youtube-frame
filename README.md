6421cc25c40242f872f959665bdc89705acbeb8f
Chris Andrejewski <christopher.andrejewski@gmail.com>
2016-03-03 22:03:55 -0500

    Real initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# youtube-frame
-Full window YouTube videos (not fullscreen, full window)
+# YouTube Frame
+
+Sometimes I want a YouTube video in a small window
+on my screen, but with YouTube you only can do
+full screen or skrink the window along with the
+comments, suggested videos, and whatever else, and
+try to hand nudge the video into place. That sucks
+so here is this.
+
+There are Chrome extensions for this, but I thought
+a small web tool is worth a shot too for cross-browser
+support plus a bookmarklet can send people here.
+
---
+++ index.js
@@ -0,0 +1,40 @@
+
+var $frame = $('#frame');
+var $input = $('#input');
+
+function youtubeVideoUrl(str) {
+  // accepts c/p urls: https://www.youtube.com/watch?v=*
+  // accepts embeds: https://www.youtube.com/embed/*
+  // accepts shortened: https://youtu.be/*
+  var id = str.split('/').pop().split('=').pop();
+  if(!id) return null;
+  return "https://www.youtube.com/embed/"+id;
+}
+
+function fitFrame() {
+  $frame.attr('width', $(window).innerWidth());
+  $frame.attr('height', $(window).innerHeight());
+}
+
+function loadFrame(url) {
+  $frame.attr('src', url);
+}
+
+$input.on('submit', function(e) {
+  e.preventDefault();
+
+  var $link = $('.link', $input);
+  var url = youtubeVideoUrl($link.val());
+
+  if(!url) {
+    var $warn = $('.warn', $input);
+    $warn.text('Not a valid Youtube video URL');
+  } else {
+    $frame.removeClass('hidden');
+    $(window).on('resize', fitFrame);
+    fitFrame();
+    loadFrame(url);
+  }
+});
+
+
